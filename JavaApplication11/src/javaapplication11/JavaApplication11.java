035e64a5608afd829239cba7c087f60879b435df
muhammed <muhamed.313185008@uhd.edu.iq>
2020-01-28 22:30:42 +0300

    this is a JOptionPane

---
+++ JavaApplication11/src/javaapplication11/JavaApplication11.java
@@ -0,0 +1,43 @@
+
+        package javaapplication11;
+        import javax.swing.*;
+
+       public class JavaApplication11 {
+
+    
+       public static void main(String[] args) {
+       String name=new String(); 
+       name= JOptionPane.showInputDialog(null,"what is your name please?"+JOptionPane.QUESTION_MESSAGE);
+      
+       int num1=0;
+       num1= Integer.parseInt(JOptionPane.showInputDialog(null,"enter the first number "+JOptionPane.QUESTION_MESSAGE));
+      
+       int num2=0; 
+       num2= Integer.parseInt(JOptionPane.showInputDialog(null,"enter the second number "+JOptionPane.QUESTION_MESSAGE));
+    
+     
+       int x=num1+num2;
+       JOptionPane.showMessageDialog(null,"welcome muhammed the sum is "+ x);
+       
+       
+       
+       
+       
+        
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+       
+}}
